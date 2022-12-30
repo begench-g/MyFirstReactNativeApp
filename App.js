eec928d4d657b0e21b56eec7288ffbc82449ee1b
BegenchGurbanmammedov <93909709+BegenchGurbanmammedov@users.noreply.github.com>
2022-12-30 17:18:13 +0500

    myFirstReactNative

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import App from './Home';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App  start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const  navigator = createStackNavigator(
+  {
+    Home:App
   },
-});
+  {
+    initialRouteName:'Home',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+)
+
+export default createAppContainer(navigator)
